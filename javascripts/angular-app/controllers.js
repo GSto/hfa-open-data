4f68a3078a51b00869090f7f60abc1c348971d14
Glenn Stovall <glennstovall@gmail.com>
2013-06-01 18:06:49 -0400

    added base angular js files

---
+++ javascripts/angular-app/controllers.js
@@ -0,0 +1,3 @@
+app.controller('MainController', ['$scope', 'DistrictMap', function($scope, DistrictMap) {
+	
+}]);
---
+++ javascripts/angular-app/services.js
@@ -0,0 +1,13 @@
+app.factory('DistrictMap', ['$http', function($http) {
+	var DistrictMap = {};
+	/*
+	$http.jsonp('url', {})
+		.success(function(data, status, headers, config) {
+
+		})
+		.error(function(data, status, headers, config) {
+			
+		});
+	*/
+	return DistrictMap;
+}]);
